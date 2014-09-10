634f4c72dcb8b7bd2c76cd3780dc46a832702313
Lingpo Huang <lingpohuang7@gmail.com>
2014-09-10 15:49:46 -0500

    Add some IO to see the actual parsing.

--- test/Persist/Mongo/SettingsSpec.hs
+++ test/Persist/Mongo/SettingsSpec.hs
@@ -2,8 +2,6 @@
 module Persist.Mongo.SettingsSpec (main, spec) where
 
 import           Data.Aeson
-import           Database.Persist
-import           Persist.Mongo.Settings
 import           Test.Hspec
 import           TestImport
 
@@ -14,8 +12,10 @@ spec :: Spec
 spec = do
   describe "testParseOnpingTagCombined" $ do
     it "should be able to parse the JSON back to a OnpingTagCombined" $ do
+      print $ toJSON $ parseOnpingTagCombined testOnpingTagCombinedJSON
       ((encode . parseOnpingTagCombined $ testOnpingTagCombinedJSON) == testOnpingTagCombinedJSON) `shouldBe` True
   describe "testParseOnpingTagCombinedEntity" $ do
     it "Should be able to parse an OnpingTagCombined entity " $ do
+      print $ toJSON $ parseOnpingTagCombinedEntity testOnpingTagCombinedEntityJSON
       ((encode . parseOnpingTagCombinedEntity $ testOnpingTagCombinedEntityJSON) == testOnpingTagCombinedEntityJSON) `shouldBe` True
 
