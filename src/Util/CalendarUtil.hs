5df963dd52dffd5eadbac37c45bb615d5c1226a0
Scott Murphy <scottmurphy09@gmail.com>
2014-09-19 20:24:16 -0500

    updated persist-mongo-template with correct calendar widget

--- src/Util/CalendarUtil.hs
+++ src/Util/CalendarUtil.hs
@@ -7,25 +7,30 @@ import           Data.Aeson.Types
 import           Data.List
 import           Data.Time
 import           Data.Time.Calendar.WeekDate
-import           Data.Traversable
+-- import           Data.Traversable
 import           Persist.Mongo.Settings
 import           Yesod                       hiding (runDB)
 
 -- | Helper functions to explicity do type conversion
 
+fullDayInSeconds :: Integer
+fullDayInSeconds =  86400
+
 isOnDuty :: MongoDBConf -> UserId -> UTCTime -> IO Bool
 isOnDuty mdbc uid time = do
   tz <- getCurrentTimeZone
-  let day = utctDay time
-  let weekDay = (third $ toWeekDate day) - 1
-  let diffTime = utctDayTime time
+  let day = utctDay time   
+  let weekDay = (third $ toWeekDate day) - 1 -- the day of the week it is, formatted like the javascript counterpart
+  let diffTime = utctDayTime time -- how many seconds into the day
   let timeZoneDiffTimes = secondsToDiffTime . toInteger $ (timeZoneMinutes tz) * 60
-  let diffTimeWTimeZone =  diffTime + timeZoneDiffTimes
+  let diffTimeWTimeZone =  fromInteger $ mod (round $ diffTime + timeZoneDiffTimes) fullDayInSeconds
   mcalObj <- runDBConf mdbc  $ getBy $ UniqueUserId uid
   case entityVal <$> mcalObj of
     Nothing -> do
       return False
-    (Just (CalendarWidget _ _ days hours mins durations allDay)) -> return $ isWithinTime timeList weekDay diffTimeWTimeZone
+    (Just (CalendarWidget _ _ days hours mins durations allDay)) -> do
+
+      return $ isWithinTime timeList weekDay diffTimeWTimeZone
       where
         hourSecs = map (*3600) hours
         minSecs = map (*60) mins
@@ -35,8 +40,13 @@ isOnDuty mdbc uid time = do
         timeList = zip4 days onDutyDiffTimes diffTimeDurations allDay
 
 isWithinTime :: [(Int, DiffTime, DiffTime, Bool)] -> Int -> DiffTime -> Bool
-isWithinTime ((day, diffStart, diffDuration, allDay):[]) targetDay targetDiffTime = (day == targetDay && diffStart <= targetDiffTime && diffStart + diffDuration >= targetDiffTime) || (allDay == True && day == targetDay)
-isWithinTime ((day, diffStart, diffDuration, allDay):list) targetDay targetDiffTime = (day == targetDay && diffStart <= targetDiffTime && diffStart + diffDuration >= targetDiffTime || isWithinTime list targetDay targetDiffTime ) || (allDay == True && day == targetDay)
+isWithinTime ((day, diffStart, diffDuration, allDay):[]) targetDay targetDiffTime = (day == targetDay &&
+                                                                                     diffStart <= targetDiffTime &&
+                                                                                     diffStart + diffDuration >= targetDiffTime) || (allDay == True &&
+                                                                                                                                     day == targetDay)
+isWithinTime ((day, diffStart, diffDuration, allDay):list) targetDay targetDiffTime = (day == targetDay &&
+                                                                                       diffStart <= targetDiffTime &&
+                                                                                       diffStart + diffDuration >= targetDiffTime || isWithinTime list targetDay targetDiffTime ) || (allDay == True && day == targetDay)
 isWithinTime [] _ _ = False
 
 third :: (a, b, c) -> c
@@ -44,36 +54,6 @@ third (_, _, t) = t
 
 -- | The following function is used to test if is OnDuty is working
 instance ToJSON CalendarWidget where
-
-testIsOnDuty :: IO ()
-testIsOnDuty = do
-  putStrLn "Function for testing is OnDuty\n"
-  (Right mConf) <- readDBConf "config.yml"
-  users <- runDBConf mConf $ selectList [] [Asc UserTagId]
-  putStrLn "User List: " -- using the userTagId
-  _ <- traverse (\user -> print (entityKey user, userTagName .entityVal $ user)) users
-  t <- liftIO $ getCurrentTime
-  let tList = t:(buildTimeList t)
-  putStrLn "Day List: "
-  _ <- traverse (\tm -> print tm) tList
-  putStrLn "Result List: "
-  _ <- traverse (\time -> checkDailyDuty mConf users time) tList
-  putStrLn "\n =============Done=============="
-
-checkDailyDuty :: MongoDBConf -> [Entity UserTag] -> UTCTime -> IO [()]
-checkDailyDuty conf users t = do
-   putStrLn "\nCurrent Date is: " >> (print t)
-   traverse (checkOnDuty conf t) users
-
-buildTimeList :: UTCTime -> [UTCTime]
-buildTimeList t = timeIncreament t <$> lst
-    where lst = [1,2,3,4,5,6,7]
-          timeIncreament tm n = UTCTime (addDays n (utctDay tm)) (utctDayTime tm)
-
-checkOnDuty :: MongoDBConf -> UTCTime -> Entity UserTag -> IO ()
-checkOnDuty conf t ut = do
-     rslt <- isOnDuty conf (userTagUser . entityVal $ ut) t
-     print (userTagName . entityVal $ ut, rslt)
 --   let day = utctDay t
 --   let weekDay = (third $ toWeekDate day) - 1
 --   let diffTime = utctDayTime t
